b211ae488706a1bb216151716143040435d1a9da
Caio Fuccio <caio.fuccio@gmail.com>
2020-12-19 16:54:40 -0300

    Added round counter and a endgame screen after round 5,  with restart button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const game = () => {
   let playerScore = 0;
   let computerScore = 0;
-  let gameRound = 0;
+  let gameRound = 1;
 
   // Intro screen transition to match screen
   const startGame = () => {
@@ -28,7 +28,6 @@ const game = () => {
         //Computer random Hand
         const randomChoice = Math.floor(Math.random() * computerChoices.length);
         const computerHand = computerChoices[randomChoice];
-        console.log(computerHand);
 
         // Call compareHands function
         compareHands(playerHand, computerHand);
@@ -43,8 +42,10 @@ const game = () => {
     // Comparing who wins the round
     if (computerHand === playerHand) {
       gameRound++;
-      resultText.textContent = "It's a tie";
+      resultText.textContent = "It's a tie!";
       updateScore();
+      updateRound();
+      endGame();
       return;
     } else if (
       (computerHand === "rock" && playerHand === "paper") ||
@@ -53,8 +54,10 @@ const game = () => {
     ) {
       playerScore++;
       gameRound++;
-      resultText.textContent = "You won the round";
+      resultText.textContent = `Computer chooses ${computerHand}. You won the round!`;
       updateScore();
+      updateRound();
+      endGame();
       return;
     } else if (
       (computerHand === "paper" && playerHand === "rock") ||
@@ -63,8 +66,10 @@ const game = () => {
     ) {
       computerScore++;
       gameRound++;
-      resultText.textContent = "You lose the round";
+      resultText.textContent = `Computer chooses ${computerHand}. You lose the round!`;
       updateScore();
+      updateRound();
+      endGame();
       return;
     }
   };
@@ -78,10 +83,83 @@ const game = () => {
     cScore.textContent = computerScore;
   };
 
+  // Update the round counter
+  const updateRound = () => {
+    const roundCounter = document.querySelector(".round p");
+
+    roundCounter.textContent = gameRound;
+  };
+
+  // Match Screen transition to Game Over Screen
+  const endGame = () => {
+    const gameoverScreen = document.querySelector(".gameover");
+    const matchScreen = document.querySelector(".match");
+    const roundCounter = document.querySelector(".round p");
+    const winnerText = document.querySelector(".winner p");
+
+    if (gameRound === 6) {
+      roundCounter.textContent = "End";
+
+      matchScreen.classList.remove("fadeIn");
+      matchScreen.classList.add("fadeOut");
+      matchScreen.style.pointerEvents = "none";
+
+      setTimeout(() => {
+        gameoverScreen.classList.add("fadeIn");
+      }, 500);
+
+      if (playerScore === computerScore) {
+        winnerText.textContent = "It's a draw!";
+      } else if (playerScore > computerScore) {
+        winnerText.textContent = "You won the game!";
+      } else {
+        winnerText.textContent = "You lose the game!";
+      }
+
+      return;
+    } else {
+      return;
+    }
+  };
+
+  // Restart the Game
+  const restartGame = () => {
+    const restartButton = document.querySelector(".winner button");
+
+    const gameoverScreen = document.querySelector(".gameover");
+    const matchScreen = document.querySelector(".match");
+
+    const roundCounter = document.querySelector(".round p");
+    const pScore = document.querySelector(".player-score p");
+    const cScore = document.querySelector(".computer-score p");
+    const resultText = document.querySelector(".result h3");
+
+    restartButton.addEventListener("click", () => {
+      gameoverScreen.classList.remove("fadeIn");
+      gameoverScreen.classList.add("fadeOut");
+
+      //Resets all variables
+      playerScore = 0;
+      computerScore = 0;
+      gameRound = 1;
+
+      pScore.textContent = playerScore;
+      cScore.textContent = computerScore;
+      roundCounter.textContent = gameRound;
+      resultText.textContent = "Choose a hand";
+
+      setTimeout(() => {
+        matchScreen.classList.remove("fadeOut");
+        matchScreen.classList.add("fadeIn");
+        matchScreen.style.pointerEvents = "all";
+      }, 700);
+    });
+  };
+
   // Calling inner functions
   startGame();
   playMatch();
-  updateScore();
+  restartGame();
 };
 
 // Calling main function
